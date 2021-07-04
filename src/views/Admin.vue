47f7ba4c1f18357f1da12747064a2dd9d3851653
DavidSC <david@wbcboxing.com>
2021-07-04 03:31:28 -0500

    avatar y botones de carga de img

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,3 +1,41 @@
 <template>
-    <h1>Admin</h1>
+   <v-layout justify-center>
+       <v-flex xs6>
+            <v-card class="text-xs-center">
+                   <v-card-text>
+                       <v-avatar>
+                          <img :src="usuario.foto" alt="avatar">
+                        </v-avatar>
+                   </v-card-text>
+                    <v-card-text>
+                        <h3>{{usuario.nombre}}</h3>
+                    </v-card-text>
+                    <v-card-text>
+                        <input type="file" ref="boton" class="d-none" @change="buscarImagen($event)">
+                        <v-btn color="primary" @click="$refs.boton.click()">Buscar imagen</v-btn>
+                        <v-btn color="secondary" :disabled="file === null">Subir imagen</v-btn>
+                    </v-card-text>
+            </v-card>
+       </v-flex>
+   </v-layout>
 </template>
+
+<script>
+import {mapState} from 'vuex'
+export default {
+    data() {
+        return {
+            file: null
+        }
+    },
+    computed:{
+        ...mapState(['usuario'])
+    },
+    methods: {
+        buscarImagen(event) {
+            console.log(event.target.files[0]);
+            this.file = event.target.files[0];
+        }
+    },
+}
+</script>
