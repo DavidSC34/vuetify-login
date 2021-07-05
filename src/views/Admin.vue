c9d96c05250deca35fbde5d300b9d1870cc05795
DavidSC <david@wbcboxing.com>
2021-07-04 19:38:29 -0500

    subir img al storage+ animacion

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -13,7 +13,12 @@
                     <v-card-text>
                         <input type="file" ref="boton" class="d-none" @change="buscarImagen($event)">
                         <v-btn color="primary" @click="$refs.boton.click()">Buscar imagen</v-btn>
-                        <v-btn color="secondary" :disabled="file === null">Subir imagen</v-btn>
+                        <v-btn 
+                            color="secondary" 
+                            :disabled="file === null"
+                            @click="subirImagen()"
+                            :loading="loading"
+                            >Subir imagen</v-btn>
                     </v-card-text>
                     <v-card-text v-if="file">
                         <h4>{{file.name}}</h4>
@@ -26,11 +31,13 @@
 
 <script>
 import {mapState} from 'vuex'
+import {firebase,storage,db} from '@/firebase'
 export default {
     data() {
         return {
             file: null,
-            urlTemporal:''
+            urlTemporal:'',
+            loading:false
         }
     },
     computed:{
@@ -46,6 +53,26 @@ export default {
             reader.onload = (e)=>{
                 //console.log(e.target.result);
                 this.urlTemporal = e.target.result;
+            }
+        },
+        async subirImagen(){
+
+            try {
+                this.loading = true;
+                const refImagen = storage.ref().child(this.usuario.email).child('foto_perfil');
+                const res = await refImagen.put(this.file);
+                console.log(res);
+                //Para reemaplazar la imagen ne la base de datos
+                const urlDescarga = await refImagen.getDownloadURL();
+                this.usuario.foto = urlDescarga;
+                await db.collection('usuarios').doc(this.usuario.uid).update({
+                    foto:urlDescarga
+                })
+            } catch (error) {
+                console.log(error);
+            }finally{
+                //cuando termine o falle termine la animacion de subir
+                this.loading = false;
             }
         }
     },
